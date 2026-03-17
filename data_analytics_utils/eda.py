35f4647e4307b089e3e0b6d0f58a788f1c93a8dd
anshuludhani7 <anshul.udhani@alumni.iitd.ac.in>
2026-03-17 18:22:39 +0530

    Update eda.py

--- data_analytics_utils/eda.py
+++ data_analytics_utils/eda.py
@@ -1,1 +1,19 @@
-
+def explain_data(dataframe):
+  ''' 
+  Returns a dataframe with columns such as shape, data types,
+  missing values and descriptive stats for all columns of input dataframe. 
+  
+  '''
+  
+  print(f"The data has {dataframe.shape[0]} rows and {dataframe.shape[1]} columns")
+  
+  print('Below are the column wise data-types,missing values, unique level and descriptive stats of the data')
+  
+  dtypes = dataframe.dtypes
+  missing_values = dataframe.isnull().sum()
+  unique_values = dataframe.nunique()
+  describe = dataframe.describe(include='all').transpose()
+  details = pd.concat([dtypes,missing_values,unique_values],axis =1,\
+                      keys=['dtypes','missing_values','unique_values'])
+  details = pd.concat([details,describe],axis=1)
+  return details
